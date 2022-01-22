3a243df34db760aa97ff9f2447e320d46bcbece3
Alik Moldovanu <alexei.moldovanu@ebs-integrator.com>
2022-01-22 19:00:33 +0200

    Init project

---
+++ RutaMD/RutaMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RutaMDApp.swift
+//  RutaMD
+//
+//  Created by Alik Moldovanu on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RutaMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
